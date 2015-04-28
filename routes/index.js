a000981fac1ea120e3209124f7281b384c49560f
Nemo <me@nemoge.com>
2015-04-28 14:41:14 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+	res.render('signup');
 });
 
 module.exports = router;
